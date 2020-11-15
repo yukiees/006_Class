19f0bd1ad9e814afc32a728110bbd833b31c3a46
yukiees <yukiii8989@gmail.com>
2020-11-15 20:49:35 +0900

    Initial Commit

---
+++ 006_Class/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  006_Class
+//
+//  Created by 松島優希 on 2020/11/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
