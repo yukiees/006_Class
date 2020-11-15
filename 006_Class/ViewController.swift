faa5e033523730a9082fc7a047c3020728365bf7
yukiees <yukiii8989@gmail.com>
2020-11-16 01:25:26 +0900

    UI

--- 006_Class/ViewController.swift
+++ 006_Class/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var courseLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func mae(_ sender: Any) {
+    }
+    
+    @IBAction func tsugi(_ sender: Any) {
+    }
+    
 
 }
 
---
+++ 006_Class/mentor.swift
@@ -0,0 +1,12 @@
+//
+//  mentor.swift
+//  006_Class
+//
+//  Created by 松島優希 on 2020/11/16.
+//
+
+import UIKit
+
+class Mentor{
+    
+}
