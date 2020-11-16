dfe9065b1432d28db5087bca7448103634276e39
yukiees <yukiii8989@gmail.com>
2020-11-16 16:19:24 +0900

    Class comp

--- 006_Class/ViewController.swift
+++ 006_Class/ViewController.swift
@@ -8,19 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var mentorArray: [Mentor] = []
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var courseLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
     
+    var index: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mentorArray.append(Mentor(name: "ながた", course: "iPhone", imageName: "nagata.jpg"))
+        mentorArray.append(Mentor(name: "りょう", course: "Unity", imageName: "ryo.jpg"))
+        mentorArray.append(Mentor(name: "たいてぃ", course: "WebD,WebS", imageName: "taithi.jpg"))
+        setUI()
     }
+    
+    func setUI(){
+        imageView.image = mentorArray[index].getImage()
+        nameLabel.text = mentorArray[index].name
+        courseLabel.text = mentorArray[index].course
+    }
+    
     @IBAction func mae(_ sender: Any) {
+        if index == 0{
+            index = mentorArray.count-1
+        }else{
+            index -= 1
+        }
+        setUI()
     }
     
     @IBAction func tsugi(_ sender: Any) {
+        if index == mentorArray.count-1{
+            index = 0
+        }else{
+            index += 1
+        }
+        setUI()
     }
     
 
--- 006_Class/mentor.swift
+++ 006_Class/mentor.swift
@@ -9,4 +9,17 @@ import UIKit
 
 class Mentor{
     
+    var name: String!
+    var course: String!
+    var imageName: String!
+    
+    init(name: String, course: String, imageName: String){
+        self.name = name
+        self.course = course
+        self.imageName = imageName
+    }
+    
+    func getImage() -> UIImage {
+        return UIImage(named: imageName)!
+    }
 }
